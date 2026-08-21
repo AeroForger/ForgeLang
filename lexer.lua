95b1ad253bf9d2c38c85a53491653e9bd7ed4732
AeroForger <AeroForgery@proton.me>
2026-08-21 16:48:46 +0400

    Add files via upload

---
+++ lexer.lua
@@ -0,0 +1,55 @@
+local KeyWords = {
+    ["Number"] = "NUMBER_TYPE" 
+}
+local Symbols = {
+    ["="] = "EQUALS",
+    [";"] = "SEMICOLON"
+}
+local function tokenize(source)
+    local tokens = {}
+    --[[
+        local file, err = io.open("ForgeLang.anvil", "r")
+        if not file then
+            print("Could not open file: " .. err)
+            return
+        end
+        local content = file:read("*a")
+        We will Move this to Main.lua i commented it so we dont lose it
+    --]]
+    local i = 1
+    while i <= #source do
+        local ch = source:sub(i, i)
+
+        if ch == " " or ch == "\n" or ch == "\t" then
+            i = i + 1
+        elseif ch == "=" then
+            table.insert(tokens, "EQUALS")
+            i = i + 1
+        elseif ch == ";" then
+            table.insert(tokens, "SEMICOLON")
+            i = i + 1
+        else
+            local word = ""
+            while i <= #source and source:sub(i, i) ~= " " and source:sub(i, i) ~= "\n" and source:sub(i, i) ~= "\t" and source:sub(i, i) ~= "=" and source:sub(i, i) ~= ";" do
+                word = word .. source:sub(i, i)
+                i = i + 1
+            end
+
+            if word == "" then
+                i = i + 1
+            elseif KeyWords[word] then
+                table.insert(tokens, KeyWords[word])
+            elseif string.match(word, "^%d+$") then
+                table.insert(tokens, 'NUMBER("' .. word .. '")')
+            else
+                table.insert(tokens, 'IDENTIFIER("' .. word .. '")')
+            end
+        end
+    end
+    return tokens
+end
+local tokens = tokenize("Number X = 2;")
+
+for i, token in ipairs(tokens) do
+    print(i, token)
+end
